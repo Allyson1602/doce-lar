e568488f9d2bce30a9ebee105589bf02efb6e7a8
Allyson1602 <aathyrson@yahoo.com.br>
2022-10-04 22:09:34 -0300

    feat: criação de funcionalidade para ordenação de aluguéis

--- components/order.tsx
+++ components/order.tsx
@@ -1,5 +1,7 @@
 import { Chip } from "@mui/material";
 import { FC, useState } from "react";
+import { DataOrder } from "../interfaces/orders";
+import { dataOrder } from "../model/order";
 import { ChipsStyled, OrderStyled, TitleStyled } from "../styles/components/order";
 
 interface OrderProps {
@@ -7,7 +9,43 @@ interface OrderProps {
 }
 
 const Order: FC<OrderProps> = (props) => {
-    const [orders, setOrders] = useState();
+    const [orders, setOrders] = useState<DataOrder>(dataOrder);
+
+    const handleClickRelevance = () => {
+        let changeOrders = orders;
+        changeOrders.relevance = !orders.relevance;
+
+        if(changeOrders.relevance === true) {
+            changeOrders.higherPrice = false;
+            changeOrders.lowestPrice = false;
+        }
+
+        setOrders({...changeOrders});
+    };
+
+    const handleClickHigherPrice = () => {
+        let changeOrders = orders;
+        changeOrders.higherPrice = !orders.higherPrice;
+
+        if(changeOrders.higherPrice === true) {
+            changeOrders.relevance = false;
+            changeOrders.lowestPrice = false;
+        }
+
+        setOrders({...changeOrders});
+    };
+
+    const handleClickLowestPrice = () => {
+        let changeOrders = orders;
+        changeOrders.lowestPrice = !orders.lowestPrice;
+
+        if(changeOrders.lowestPrice === true) {
+            changeOrders.relevance = false;
+            changeOrders.higherPrice = false;
+        }
+
+        setOrders({...changeOrders});
+    };
 
     return (
         <OrderStyled>
@@ -17,20 +55,20 @@ const Order: FC<OrderProps> = (props) => {
                 <Chip
                     size="small"
                     label="relevância"
-                    variant={filters.animals ? "filled" : "outlined"}
-                    onClick={handleClickAnimals}
+                    variant={orders.relevance ? "filled" : "outlined"}
+                    onClick={handleClickRelevance}
                 />
                 <Chip
                     size="small"
                     label="maior preço"
-                    variant={filters.kids ? "filled" : "outlined"}
-                    onClick={handleClickKids}
+                    variant={orders.higherPrice ? "filled" : "outlined"}
+                    onClick={handleClickHigherPrice}
                 />
                 <Chip
                     size="small"
                     label="menor preço"
-                    variant={filters.garage ? "filled" : "outlined"}
-                    onClick={handleClickGarage}
+                    variant={orders.lowestPrice ? "filled" : "outlined"}
+                    onClick={handleClickLowestPrice}
                 />
             </ChipsStyled>
         </OrderStyled>
--- styles/components/order.tsx
+++ styles/components/order.tsx
@@ -8,4 +8,24 @@ export const TitleStyled = styled(Typography)`
     color: ${props => (props.theme as ITheme).palette.common.white};
 `;
 
-export const ChipsStyled = styled(Stack)``;
+export const ChipsStyled = styled(Stack)`
+    flex-direction: row;
+    justify-content: space-around;
+    gap: 3px;
+
+    .MuiChip-filled {
+        background-color: ${props => (props.theme as ITheme).palette.common.white};
+        color: ${props => (props.theme as ITheme).palette.secondary.main};
+        border: 1px solid transparent;
+        
+        :hover {
+            opacity: 0.8;
+            background-color: ${props => (props.theme as ITheme).palette.common.white};
+        };
+    };
+
+    .MuiChip-outlined {
+        color: ${props => (props.theme as ITheme).palette.common.white};
+        border: 1px solid ${props => (props.theme as ITheme).palette.common.white};
+    };
+`;
